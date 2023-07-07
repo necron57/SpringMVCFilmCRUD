cd4a9597fdc11d18585f0d380e46cdc534885d8b
Cortnie Barrow <mommy24227@gmail.com>
2023-07-07 18:31:15 -0400

    Full-Stack smoke test

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -2,15 +2,23 @@ package com.skilldistillery.film.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.data.FilmDAO;
+
 
 @Controller
 public class FilmController {
 
-	
 	@Autowired
 	private FilmDAO filmDao;
-	
-	
+
+	@RequestMapping(path = {"/","home.do"})
+	public ModelAndView goHome() {
+		ModelAndView mv = new ModelAndView("WEB-INF/home.jsp");
+		return mv;
+	}
 }
