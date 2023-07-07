722bdfeb0e41502b4dfcfe583b30f5822bb72c65
necron57 <jacoba1995@yahoo.com>
2023-07-07 16:49:49 -0600

    Create Actor entity, configure DAO/Impl

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -1,5 +1,27 @@
 package com.skilldistillery.film.data;
 
-public interface FilmDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmDAO {
+	public Film findFilmById(int filmId) throws SQLException;
+
+	public Actor findActorById(int actorId) throws SQLException;
+
+	public List<Film> findFilmsByKeyword(String keyword);
+
+	public List<Actor> findActorsByFilmId(int filmId);
+
+	public List<Film> findFilmsByActorId(int actorId);
+
+	public Actor createActor(Actor actor);
+
+	public Film createFilm(Film film);
+
+	public boolean saveFilm(Film film);
+
+	public boolean deleteFilm(Film film);
 }
--- src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
@@ -1,5 +1,78 @@
 package com.skilldistillery.film.data;
 
-public class FilmDaoJdbcImpl implements FilmDAO{
+import java.sql.SQLException;
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public class FilmDaoJdbcImpl implements FilmDAO {
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=US/Mountain";
+	private static final String userName = "student";
+	private static final String password = "student";
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("Error loading MySQL Driver");
+			throw new RuntimeException("Unable to load MySQL Driver class");
+		}
+	}
+
+	@Override
+	public Film findFilmById(int filmId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Actor findActorById(int actorId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Film> findFilmsByKeyword(String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Film> findFilmsByActorId(int actorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Actor createActor(Actor actor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Film createFilm(Film film) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean saveFilm(Film film) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteFilm(Film film) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
