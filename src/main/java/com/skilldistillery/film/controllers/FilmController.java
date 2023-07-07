1be4456a45798166f29e6cdec0694582ec957d44
necron57 <jacoba1995@yahoo.com>
2023-07-07 15:52:42 -0600

    create FilmDAO/Impl

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,16 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.skilldistillery.film.data.FilmDAO;
+
+@Controller
 public class FilmController {
 
+	
+	@Autowired
+	private FilmDAO filmDao;
+	
+	
 }
